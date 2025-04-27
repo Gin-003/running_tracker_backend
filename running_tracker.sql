406acc8f9b82e0dc8b83fe6f9a293a55c81fb078
Thein Htet Soe <theinhtetsoe6@gmail.com>
2025-04-27 10:00:28 +0630

    added sql file

---
+++ running_tracker.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 27, 2025 at 05:29 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `running_tracker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `created_at`) VALUES
+(1, 'theinhtetsoe', '$2y$10$QmqLtCFW20Ku1QpDYOLgdefHaidU8URF/7NRncSv/f/01KoxYALdy', 'theinhtetsoe@kbtc.edu.mm', '2025-04-22 16:17:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workouts`
+--
+
+CREATE TABLE `workouts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `distance` float NOT NULL,
+  `duration` int(11) NOT NULL,
+  `average_speed` float NOT NULL,
+  `calories_burned` float NOT NULL,
+  `start_location` varchar(100) NOT NULL,
+  `end_location` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `workouts`
+--
+
+INSERT INTO `workouts` (`id`, `user_id`, `distance`, `duration`, `average_speed`, `calories_burned`, `start_location`, `end_location`, `created_at`) VALUES
+(1, 1, 5.2, 1800, 10.4, 312, '16.8209,96.1553', '16.8210,96.1554', '2025-04-18 15:02:41'),
+(2, 1, 5.2, 1800, 10.4, 312, '16.8209,96.1553', '16.8210,96.1554', '2025-04-26 16:53:59');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `username_2` (`username`),
+  ADD KEY `email_2` (`email`);
+
+--
+-- Indexes for table `workouts`
+--
+ALTER TABLE `workouts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `workouts`
+--
+ALTER TABLE `workouts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
